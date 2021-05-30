070fd9c46bf8c3496189d572fe0f1c1cf2551c85
Pouya Vedadiyan <Vedadiyan@gmail.com>
2021-05-30 23:10:02 +0430

    Initial Commit

---
+++ Events.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Events.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Events.Core.Event events = new Core.Event(TimeSpan.FromSeconds(5));
+            events.Point += (sender, e)=> {
+                Console.WriteLine("Hit");
+            };
+            events.Start(false);
+            Thread.Sleep(15000);
+            events.Stop();
+            Console.WriteLine("Stopped");
+            Console.ReadLine();
+        }
+    }
+}
